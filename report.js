ed4646eb35a05b7916e001b1d6251824ec02bc7f
laughingbiscuit <sean@laughingbiscuit.com>
2023-05-25 22:42:43 +0000

    arbitrary brand

--- report.js
+++ report.js
@@ -5,7 +5,7 @@ var options = {
         jsonFile: 'report.json',
         output: 'result/index.html',
         reportSuiteAsScenarios: true,
-        brandTitle: '☺',
+        brandTitle: '♫',
         columnLayout: 1
     };
 
