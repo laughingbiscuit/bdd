0dc4d332c18bfd49bdd9f40f35a61ab693801748
laughingbiscuit <sean@laughingbiscuit.com>
2023-05-25 21:55:58 +0000

    run docker on host net

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ set -e
 # run cucumber
 
 docker rm -f browser
-docker run --name browser -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-chrome:latest
+docker run --net=host --name browser -d --shm-size="2g" selenium/standalone-chrome:latest
 sleep 5
 mkdir -p result
 cucumber --publish-quiet "$@" -f pretty -f json -o report.json || true
